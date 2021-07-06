2376e4b86678fe2257690fa3bff9c40bb3b84b1b
Giorgio Andrei <cipuandrei@libero.it>
2021-07-06 19:17:59 +0200

    exercise-done

--- script.js
+++ script.js
@@ -4,8 +4,13 @@
 3. stampa la lista ordinata alfabeticamente sia in console che in pagina
 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova */
 
+
+
 var lastNamesList = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
 console.log(lastNamesList)
+
+var printList = document.getElementById('list')
+printList.innerHTML = '1) La lista precedente era: ' + lastNamesList + '.';
 
 do {
     var userLastName = prompt('Inserisci il tuo cognome').trim()
@@ -16,11 +21,20 @@ var userLastNameCapitalized = userLastName.charAt(0).toUpperCase() + userLastNam
 var a = lastNamesList.push(userLastNameCapitalized);
 console.log(a)
 
+var printListWithYou = document.getElementById('you-in-list')
+printListWithYou.innerHTML = '2) Adesso anche "' + userLastNameCapitalized + '" risulta in lista: ' + lastNamesList + '.';
+
 var b = lastNamesList.sort();
 console.log(b)
+
+var listInAlphabeticalOrder = document.getElementById('list-in-alphabetical-order')
+listInAlphabeticalOrder.innerHTML = '3) La nuova lista, partendo in in ordine alfabetico è: ' + lastNamesList + '.';
 
 var c = lastNamesList.length;
 console.log(c)
 
 var d = lastNamesList.indexOf(userLastNameCapitalized) + 1;
 console.log(d)
+
+var yourPositionInList = document.getElementById('your-position-in-list')
+yourPositionInList.innerHTML = '4) Lei è il numero ' + d + ' in lista.';
